96aa1a2b6dcb1881ea3fb1439db74f29a62b728c
unknown <Medici305>
2021-03-12 02:03:19 +0000

    4. Added the filter functionality.

--- app.js
+++ app.js
@@ -2,13 +2,14 @@
 const todoInput = document.querySelector('.todoInput');
 const todoSubmit = document.querySelector('.todoSubmit');
 const todoList = document.querySelector('.todoList');
+const todoFilter = document.querySelector('.footer');
+let currentIndex = 0;
 
 // Functions
 
 // 1. Add todo
 const addToDo = (e) => {
     e.preventDefault();
-    console.log('submit');
     // Create todo Div
     const todoDiv = document.createElement('div');
     todoDiv.classList = 'todo-div';
@@ -28,12 +29,15 @@ const addToDo = (e) => {
     trashBtn.innerHTML = '<i class="fa fa-times"></i>';
     todoDiv.appendChild(trashBtn);
     // Append div to ul
-    todoList.appendChild(todoDiv);
+    //todoList.appendChild(todoDiv);
+    todoList.insertBefore(todoDiv, todoList.childNodes[currentIndex]);
+    // increment index 
+    currentIndex++;
     todoInput.value = '';
 }
 
 // 2. Remove todo
-const removeTodo = (e) => {
+const removeTodo = e => {
     e.preventDefault();
     const item = e.target;
 
@@ -52,6 +56,36 @@ const removeTodo = (e) => {
     }
 }
 
+// 3. Filter todo list depending whats clicked on.
+const filterTodo = e => {
+    const list = todoList.childNodes;
+    list.forEach(item => {
+        if (item.classList.contains('todo-div')) {
+            const lineClass = item.childNodes[1].classList.contains('line');
+            switch(e.target.getAttribute('value')) {
+                case 'all':
+                    item.style.display = 'flex';
+                    break;
+                case 'completed':
+                    if(lineClass) {
+                        item.style.display = 'flex';
+                    } else {
+                        item.style.display = 'none';
+                    }
+                    break;
+                case 'uncompleted':
+                    if(!lineClass) {
+                        item.style.display = 'flex';
+                    } else {
+                        item.style.display = 'none';
+                    }
+                    break;
+            }
+        }
+    })
+}
+
 // Event Listeners
 todoSubmit.addEventListener('click', addToDo);
 todoList.addEventListener('click', removeTodo);
+todoFilter.addEventListener('click', filterTodo)
